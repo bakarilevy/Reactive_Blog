6cae7eeff49e2ca17bf96cf3f9aa61655cc0edcd
Bakari A Levy <s314856@corp.aepsc.com>
2019-11-27 07:10:38 -0500

    Added basic functionality forform next planning to implement Multiple pages

--- reactive-blog/src/components/PostForm.js
+++ reactive-blog/src/components/PostForm.js
@@ -4,24 +4,31 @@ class PostForm extends Component {
 
     constructor(props) {
         super(props);
-    //    this.handleChange = this.handleChange.bind(this);
-    //    this.handleSubmit = this.handleSubmit.bind(this);
+
+        this.state = {
+            value: "Please enter your contact information here..."
+        };
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
     }
 
-    //handleChange(event) {}
+    handleChange(event) {
+        this.setState({ value: event.target.value});
+    }
 
-    //handleSubmit(event) {}
+    handleSubmit(event) {
+        alert('A request has been submitted: ' + this.state.value);
+        event.preventDefault();
+    }
 
     render() {
         return(
-            <form>
+            <form onSubmit={this.handleSubmit}>
                 <label>Test Label
-                    <select>
-                        <option value="green">Green</option>
-                        <option value="red">Red</option>
-                        <option value="blue">Blue</option>
-                    </select>
+                        <textarea value={this.state.value} onChange={this.handleChange}/>
                 </label>
+                <input type='submit' value='Submit'/>
             </form>
         );
     }
