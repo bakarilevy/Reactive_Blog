39acf208cb387a6babee6626c3a00b2ffe07bdb7
Bakari A Levy <s314856@corp.aepsc.com>
2019-12-12 12:05:14 -0500

    Managed to expand upon the download component to search for a file first.

--- reactive-blog/src/components/download/Download.js
+++ reactive-blog/src/components/download/Download.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import { Route, BrowserRouter, Link } from 'react-router-dom';
 
 class Download extends Component  {
 
@@ -8,11 +7,14 @@ class Download extends Component  {
         this.state = {
             search: '',
             url: 'http://localhost:8080/downloadFile/',
-            href: ''
+            href: '',
+            fileFound: false
         };
         this.getFile = this.getFile.bind(this);
         this.handleChange = this.handleChange.bind(this);
         this.testDownload = this.testDownload.bind(this);
+        this.testFunction = this.testFunction.bind(this);
+        this.sleep = this.sleep.bind(this);
     }
 
     //I need to create a field to enter text that will post a request to the backend capture the file
@@ -20,6 +22,10 @@ class Download extends Component  {
 
     handleChange(event) {
         this.setState({[event.target.name]: event.target.value});
+    }
+
+    sleep(ms) {
+        return new Promise(resolve => setTimeout(resolve, ms));
     }
 
     getFile() {
@@ -34,29 +40,52 @@ class Download extends Component  {
 
     testDownload() {
         const newHref = this.state.url + this.state.search;
-        fetch(
-            this.state.url + this.state.search,
-            {
-                method: 'GET'
-            }
-        )
+        // fetch(
+        //     this.state.url + this.state.search,
+        //     {
+        //         method: 'GET'
+        //     }
+        // )
         this.setState({href: newHref});
         
     }
-//Use the router to launch the file download to a new link
+
+
+
+    testFunction()  {
+        let url = this.state.url + this.state.search;
+        let request = new Request(url);
+        fetch(request)
+        .then((response)=>{
+            if(response.ok){
+                this.setState({fileFound: true});
+            }
+        });
+        this.sleep(2000).then(()=>{
+            if(this.state.fileFound){
+                alert("File was found.");
+            } else {
+                alert("File was not found.");
+            }
+        })
+    }
+
+
+//Component mount should get all file names and render on screen with a button next
+//to each entry to download specific file
  //href={this.state.url}
 
     render(){
         return(
             <div className="MainForm">
             <div>
-                <label>Download File
+                <label>Search File to Download
                     <input type='text' name='search' onChange={this.handleChange} value={this.state.search}></input>
-                    <button onClick={this.testDownload}>Download</button>
+                    <button onClick={this.testFunction}>Search</button>
                 </label>
             </div>
             <div className="TestButton">
-                <button><a href={this.state.href} download={this.state.href}>Test</a></button>
+                <button onClick={this.testDownload}><a href={this.state.href} download={this.state.href}>Test</a></button>
             </div>
             </div>
         );
