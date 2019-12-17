daadc45dc5c1b1da0f04e81b4c86719654df3389
Bakari A Levy <s314856@corp.aepsc.com>
2019-12-17 11:44:48 -0500

    Refactoring navbar to render using the routes component

--- reactive-blog/src/components/layout/navbar.js
+++ reactive-blog/src/components/layout/navbar.js
@@ -1,12 +1,23 @@
 import React, {Component} from 'react';
 import './navbar.css';
+import Routes from '../routes';
 class Navbar extends Component {
 
     constructor(props) {
         super(props);
+        this.renderDefault = this.renderDefault.bind(this);
     }
 
         //May refactor this to use the browser router links...or maybe imbed it...?
+    renderDefault() {
+        return(
+            <div className="top-navbar">
+            <a href="http://localhost:3000">Main</a>
+            <a href="http://localhost:3000/views">Views</a>
+            <a href="http://localhost:3000/upload">Upload</a>
+        </div>
+        );
+    }
 
     render() {
         return(
